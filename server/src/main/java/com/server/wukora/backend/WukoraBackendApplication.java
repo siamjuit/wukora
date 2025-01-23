b6ec3b32a998845b0b066d8b251918dbeb5aca49
Naman Tomar <namantomar1453@gmail.com>
2025-01-24 00:24:33 +0530

    refractor init

---
+++ server/src/main/java/com/server/wukora/backend/WukoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.server.wukora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WukoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WukoraBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/server/wukora/backend/WukoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.wukora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WukoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
